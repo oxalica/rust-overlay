dfb320148038ede16f05dbdfa6b3ad59cea19f6b
oxalica <oxalicc@pm.me>
2021-01-16 01:14:03 +0800

    Wrap cargo-miri for rust-src

---
+++ cargo-miri-wrapper.sh
@@ -0,0 +1,11 @@
+#!@bash@ -e
+src_dir="@out@/lib/rustlib/src/rust/library"
+if [[ ! -v XARGO_RUST_SRC ]]; then
+    if [[ ! -d "$src_dir" ]]; then
+        echo '`rust-src` is required by miri but not installed.' >&2
+        echo 'Please either install component `rust-src` or set `XARGO_RUST_SRC`.' >&2
+        exit 1
+    fi
+    export XARGO_RUST_SRC="$src_dir"
+fi
+exec -a "$0" "@miri@" "$@"
