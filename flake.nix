be4045a65d3c95dd1a2e4aa7f8044c5cdca2e79d
oxalica <oxalicc@pm.me>
2021-11-22 06:55:33 +0800

    Use nixpkgs-unstable in flake input and upgrade

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
