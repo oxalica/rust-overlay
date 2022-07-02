be0b8a7cbf95d43f23ef0bfa781d644a43c75396
GitHub Action <action@github.com>
2022-07-02 02:50:47 +0000

    manifest: update

--- manifests/renames.nix
+++ manifests/renames.nix
@@ -2,4 +2,5 @@
   { clippy = "clippy-preview"; miri = "miri-preview"; rls = "rls-preview"; rustfmt = "rustfmt-preview"; }
   { clippy = "clippy-preview"; rls = "rls-preview"; rustfmt = "rustfmt-preview"; }
   { rls = "rls-preview"; }
+  { clippy = "clippy-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rustfmt = "rustfmt-preview"; }
 ]
