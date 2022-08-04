28cedcb8dfea9f1b96b0635cf99fe6bdca163f4e
oxalica <oxalicc@pm.me>
2022-08-04 15:47:22 +0800

    flake: add more supported systems where it evaluates

--- flake.nix
+++ flake.nix
@@ -16,13 +16,18 @@
     overlay = import ./.;
 
     allSystems = [
+      "aarch64-darwin"
       "aarch64-linux"
+      "armv5tel-linux"
       "armv6l-linux"
       "armv7a-linux"
       "armv7l-linux"
-      "x86_64-linux"
+      "i686-linux"
+      # "mipsel-linux" # Missing `busybox`.
+      "powerpc64le-linux"
+      "riscv64-linux"
       "x86_64-darwin"
-      "aarch64-darwin"
+      "x86_64-linux"
     ];
 
   in {
