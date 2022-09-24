6eb90123f46664ffbb550c527f656ba848718af5
GitHub Action <action@github.com>
2022-09-24 03:01:42 +0000

    manifest: update

--- manifests/renames.nix
+++ manifests/renames.nix
@@ -3,4 +3,5 @@
   { clippy = "clippy-preview"; rls = "rls-preview"; rustfmt = "rustfmt-preview"; }
   { rls = "rls-preview"; }
   { clippy = "clippy-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rustfmt = "rustfmt-preview"; }
+  { clippy = "clippy-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; rustfmt = "rustfmt-preview"; }
 ]
