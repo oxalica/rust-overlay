f0a8bd26902afd76781e3f1dc4cc48084f24c58b
oxalica <oxalicc@pm.me>
2023-04-18 00:14:26 +0800

    flake: merge systems with nixpkgs'

--- systems.nix
+++ systems.nix
@@ -3,10 +3,9 @@
   "aarch64-linux"
   "armv5tel-linux"
   "armv6l-linux"
-  "armv7a-linux"
   "armv7l-linux"
   "i686-linux"
-  # "mipsel-linux" # Missing `busybox`.
+  "mipsel-linux"
   "powerpc64le-linux"
   "riscv64-linux"
   "x86_64-darwin"
