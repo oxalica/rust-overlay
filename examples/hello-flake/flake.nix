d9c6784d44e7a513c441b87e01e5a276a1e95c4b
Hayden Brown <scratchcat458@gmail.com>
2024-09-16 14:45:20 +1000

    docs: create flake devShell example

---
+++ examples/hello-flake/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "An example devShell with flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+        devShells.default = mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.stable.latest.default
+          ]
+        }
+    );
+}
