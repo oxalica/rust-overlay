8b137260b776525542d47d3a4dbac753df478a42
GitHub Action <action@github.com>
2025-03-14 02:31:06 +0000

    manifest: update

--- manifests/renames.nix
+++ manifests/renames.nix
@@ -8,4 +8,5 @@
   { llvm-tools = "llvm-tools-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; }
   { clippy = "clippy-preview"; llvm-tools = "llvm-tools-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; rustc-codegen-cranelift = "rustc-codegen-cranelift-preview"; rustfmt = "rustfmt-preview"; }
   { clippy = "clippy-preview"; llvm-bitcode-linker = "llvm-bitcode-linker-preview"; llvm-tools = "llvm-tools-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; rustc-codegen-cranelift = "rustc-codegen-cranelift-preview"; rustfmt = "rustfmt-preview"; }
+  { clippy = "clippy-preview"; llvm-bitcode-linker = "llvm-bitcode-linker-preview"; llvm-tools = "llvm-tools-preview"; miri = "miri-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; rustc-codegen-cranelift = "rustc-codegen-cranelift-preview"; rustfmt = "rustfmt-preview"; }
 ]
