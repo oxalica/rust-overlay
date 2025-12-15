f3fa9fedc3f468d1e93a5cb02fc937d157e98ccb
oxalica <oxalicc@pm.me>
2025-12-14 20:43:37 -0500

    manifests: copy from master branch

---
+++ manifests/renames.nix
@@ -0,0 +1,12 @@
+[
+  { clippy = "clippy-preview"; miri = "miri-preview"; rls = "rls-preview"; rustfmt = "rustfmt-preview"; }
+  { clippy = "clippy-preview"; rls = "rls-preview"; rustfmt = "rustfmt-preview"; }
+  { rls = "rls-preview"; }
+  { clippy = "clippy-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rustfmt = "rustfmt-preview"; }
+  { clippy = "clippy-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; rustfmt = "rustfmt-preview"; }
+  { clippy = "clippy-preview"; llvm-tools = "llvm-tools-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; rustfmt = "rustfmt-preview"; }
+  { llvm-tools = "llvm-tools-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; }
+  { clippy = "clippy-preview"; llvm-tools = "llvm-tools-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; rustc-codegen-cranelift = "rustc-codegen-cranelift-preview"; rustfmt = "rustfmt-preview"; }
+  { clippy = "clippy-preview"; llvm-bitcode-linker = "llvm-bitcode-linker-preview"; llvm-tools = "llvm-tools-preview"; miri = "miri-preview"; rls = "rls-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; rustc-codegen-cranelift = "rustc-codegen-cranelift-preview"; rustfmt = "rustfmt-preview"; }
+  { clippy = "clippy-preview"; llvm-bitcode-linker = "llvm-bitcode-linker-preview"; llvm-tools = "llvm-tools-preview"; miri = "miri-preview"; rust-analyzer = "rust-analyzer-preview"; rust-docs-json = "rust-docs-json-preview"; rustc-codegen-cranelift = "rustc-codegen-cranelift-preview"; rustfmt = "rustfmt-preview"; }
+]
